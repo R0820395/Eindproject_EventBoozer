c6de337d757452aa891eca8cbb530a020502b80e
Dieter Daems <daemsdieter@gmail.com>
2020-12-09 20:45:15 +0100

    EventsDetail Xaml

---
+++ DAL/dbOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class dbOperations
+    {
+        public static Event eventOphalen(int eventId)
+        {
+            using (EventEntities entities = new EventEntities())
+            {
+                var query = entities.Event
+                    .Where(x => x.EventID == eventId);
+                return query.SingleOrDefault();
+            }
+        }
+        public static List<Event> eventen()
+        {
+            using (EventEntities entities = new EventEntities())
+            {
+                var query = entities.Event;
+                return query.ToList();
+            }
+        }
+
+        public static List<Notitie> notities()
+        {
+            using (EventEntities entities = new EventEntities())
+            {
+                var query = entities.Notitie;
+                return query.ToList();
+            }
+        }
+
+        /*
+        // EventTitle
+        string EventName = "Rode neuzendag";
+
+        // Notities
+        int AantalNotities = 15;
+        List<nota> notities;
+
+        // todo
+        string Event.todo.completed = "WindowClose"; //Checkbold
+        string Event.todo.completeColor = "Black"; //green
+        int Event.aantalTodos = 15;
+
+        // Locatie
+        Event.Locatie.adres, FallbackValue='Feestzaal Den Eyck'
+        string Event.Locatie.plaats = "Kasterlee";
+        string Locatie.chLocatie = "Visible";
+
+        // Klant
+        string Klant.Bedrijf = "WOKU BVBA";
+        string Klant.Naam = FallbackValue="Jonas Vermeiren";
+        string Klant.chKlant = "Visible";
+
+        //Artiesten
+        string completeColor = 'black';
+        bool allTodoCompleted = false;
+        string chArtists = 'Visible';
+
+        int Event.AantalArtiesten = 0;
+        string Event.chArtiesten = "Visible";
+
+        */
+    }
+}
---
+++ EventBooze/Events/EventDetail.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DAL;
+
+namespace EventBooze.Events
+{
+    /// <summary>
+    /// Interaction logic for EventDetail.xaml
+    /// </summary>
+    public partial class EventDetail : Window
+    {
+        public EventDetail()
+        {            
+            InitializeComponent();   
+        }
+
+        int eventID = 4;
+
+        List<Event> events = new List<Event>();
+        List<Notitie> notities = new List<Notitie>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Event Ev = dbOperations.eventOphalen(eventID);
+            
+            events = dbOperations.eventen();
+            notities = dbOperations.notities();
+            
+
+            string AantalNotities = notities.Count() + "Notes";
+            MessageBox.Show(Ev.Eventnaam);
+        }
+
+
+
+    }
+}
